2236d43d57f57a82f3edd3ac65fe9cddbd0ccf06
Moon2007 <mooninfintyalways@gmail.com>
2022-01-12 11:06:38 +0300

    C is ready

--- main.py
+++ main.py
@@ -9,6 +9,17 @@ def A(buf):
     buf[7] = buf[7] + " #             #  "
     buf[8] = buf[8] + "#               # "
 
+def C(buf):
+    buf[0] = buf[0] +"     ########  "
+    buf[1] = buf[1] +"   ##        # "
+    buf[2] = buf[2] +"  #          # "
+    buf[3] = buf[3] +" ##            "
+    buf[4] = buf[4] +" #             "
+    buf[5] = buf[5] +" ##            "
+    buf[6] = buf[6] +"   #         # "
+    buf[7] = buf[7] +"    ##     #   "
+    buf[8] = buf[8] +"      #####    "
+
 def S(buf):
     buf[0] = buf[0] + "      ############"
     buf[1] = buf[1] + "    ##            "
@@ -90,9 +101,10 @@ def K(buf):
 s = ["", "", "", "", "", "", "", "", ""]
 A(s)
 B(s)
+C(s)
 I(s)
 J(s)
 K(s)
 R(s)
-S(s)
+S(s)    
 print(*s, sep = "\n")
