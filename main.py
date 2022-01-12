d380be0d12e95f500f5d2e5d80763bc69d5067a0
Mattqx <artem.azizian@gmail.com>
2022-01-12 10:33:23 +0300

    i READY

--- main.py
+++ main.py
@@ -9,7 +9,21 @@ def A(buf):
     buf[7] = buf[7] + " #             #  "
     buf[8] = buf[8] + "#               # "
 
+def I(buf):
+    buf[0] = buf[0] + "        #         "
+    buf[1] = buf[1] + "        #         "
+    buf[2] = buf[2] + "        #         "
+    buf[3] = buf[3] + "        #         "
+    buf[4] = buf[4] + "        #         "
+    buf[5] = buf[5] + "        #         "
+    buf[6] = buf[6] + "        #         "
+    buf[7] = buf[7] + "        #         "
+    buf[8] = buf[8] + "        #         "
+
 s = ["", "", "", "", "", "", "", "", ""]
 A(s)
 A(s)
+I(s)
+I(s)
+
 print(*s, sep = "\n")
