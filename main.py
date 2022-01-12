4fe82373ea63e64c1eb2f7e84b3695a8aadbaf3c
imnotgay <espetuhova@yandex.ru>
2022-01-12 11:24:53 +0300

    V ready

--- main.py
+++ main.py
@@ -9,7 +9,18 @@ def A(buf):
     buf[7] = buf[7] + " #             #  "
     buf[8] = buf[8] + "#               # "
 
+def V(buf):
+    buf[0] = buf[0] + "#                #"
+    buf[1] = buf[1] + "##              ##"
+    buf[2] = buf[2] + " ##            ## "
+    buf[3] = buf[3] + "  ##          ##  "
+    buf[4] = buf[4] + "   ##        ##   "
+    buf[5] = buf[5] + "    ##      ##    "
+    buf[6] = buf[6] + "     ##    ##     "
+    buf[7] = buf[7] + "      ##  ##      "
+    buf[8] = buf[8] + "        ##        "
+    
 s = ["", "", "", "", "", "", "", "", ""]
 A(s)
-A(s)
+V(s)
 print(*s, sep = "\n")
